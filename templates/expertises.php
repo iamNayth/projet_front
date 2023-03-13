7311e9cce9c540b2830e8cdf40583393c9b1c161
iamNayth <contact@naythwork.fr>
2023-03-13 11:39:59 +0100

    page expertises

--- templates/expertises.php
+++ templates/expertises.php
@@ -1,9 +1,173 @@
 <?php $title = "Expertises"; ?>
 <?php ob_start(); ?>
 
-<section>
-    
+<section id="hero-expertises">
+    <div class="container-fluid h-100 pt-5 ps-5" id="bg-expertises">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1 class="text-light nord">4 grands domaines, 20 competences</h1>
+            </div>
+            <div class="col-6 d-flex justify-content-center align-items-center p-5 position-relative">
+                <div class="bloc-expertises pb-5 d-flex justify-content-center align-items-end" id="strategie">
+                    <span class="lex text-light fs-4">Stratégie</span>
+                </div>
+                <img class="position-absolute" src="../assets/icons/design-strategie.svg" style="top: 7em; left: 10.5em;">
+            </div>
+            <div class="col-6 d-flex justify-content-center align-items-center p-5 position-relative">
+                <div class="bloc-expertises pb-5 d-flex justify-content-center align-items-end" id="creation">
+                    <span class="lex text-light fs-4">Création</span>
+                </div>
+                <img class="position-absolute" src="../assets/icons/design-creation.svg" style="top: 0.8em; left: 10em;">
+            </div>
+            <div class="col-6 d-flex justify-content-center align-items-center p-5 position-relative">
+                <div class="bloc-expertises pb-5 d-flex justify-content-center align-items-end" id="developpement">
+                    <span class="lex text-light fs-4">Développement</span>
+                </div>
+                <img class="position-absolute" src="../assets/icons/design-developpement.svg" style="top: 7em; right: 10.55em;">
+            </div>
+            <div class="col-6 d-flex justify-content-center align-items-center p-5 position-relative">
+                <div class="bloc-expertises pb-5 d-flex justify-content-center align-items-end" id="webmarketing">
+                    <span class="lex text-light fs-4">Webmarketing</span>
+                </div>
+                <img class="position-absolute" src="../assets/icons/design-marketing.svg" style="top: 5em; right: 10.5em;">
+            </div>
+        </div>
+    </div>
 </section>
+<section>
+    <div class="container pt-5 pb-5">
+        <div class="row">
+            <div class="col">
+                <h2 class="nord mb-3">Strategie</h2>
+                <p class="lex mb-5">Dédié à l'accompagnement d'un projet en amont de son lancement. De la formation, à l'analyse des KPI's jusqu'au suivi au quotidien de vos projets digitaux.</p>
+                <h2 class="nord mb-3">Conquerir l'espace web !</h2>
+                <ul class="lex">
+                    <li>Formation aux outils digitaux</li>
+                    <li>Analyse data des KPI's</li>
+                    <li>Consulting digital</li>
+                </ul>
+            </div>
+            <div class="col d-flex align-items-center justify-content-center">
+                <img src="../assets/images/expertise_strategie.png" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="container pt-5 pb-5">
+        <div class="row mb-4">
+            <div class="col-6"></div>
+            <div class="col-6 text-end">
+                <h2 class="nord mb-3">Creation</h2>
+                <p class="lex">Pôle regroupant l'ensemble des services à destination de vos projets - digitaux ainsi que print - nécessitant des compétences créatives.</p>
+            </div>
+            <div class="col-3 d-flex justify-content-center align-items-center flex-column">
+                <img class="img-fluid" src="../assets/images/creation1.png">
+                <div class="line"></div>
+                <div class="d-flex align-items-center justify-content-center border border-dark lex w-100 pt-3 pb-3">DA social media</div>
+            </div>
+            <div class="col-3 d-flex justify-content-center align-items-center flex-column">
+                <img class="img-fluid" src="../assets/images/creation2.png">
+                <div class="line"></div>
+                <div class="d-flex align-items-center justify-content-center border border-dark lex w-100 pt-3 pb-3">Print / Edition</div>
+            </div>
+            <div class="col-3 d-flex justify-content-center align-items-center flex-column">
+                <img class="img-fluid" src="../assets/images/creation3.png">
+                <div class="line"></div>
+                <div class="d-flex align-items-center justify-content-center border border-dark lex w-100 pt-3 pb-3">Branding (logo, charte graphique)</div>
+            </div>
+            <div class="col-3 d-flex justify-content-center align-items-center flex-column">
+                <img class="img-fluid" src="../assets/images/creation4.png">
+                <div class="line"></div>
+                <div class="d-flex align-items-center justify-content-center border border-dark lex w-100 pt-3 pb-3">Montage vidéo / shooting photo</div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col d-flex align-items-center justify-content-end">
+                <div class="d-flex align-items-center justify-content-center border border-dark lex w-5 pt-2 pb-2 ps-5 pe-5">UX / UI Design</div>
+            </div>
+            <div class="col d-flex align-items-center justify-content-center">
+                <div class="d-flex align-items-center justify-content-center border border-dark lex w-5 pt-2 pb-2 ps-5 pe-5">Packaging</div>
+            </div>
+            <div class="col d-flex align-items-center justify-content-start">
+                <div class="d-flex align-items-center justify-content-center border border-dark lex w-5 pt-2 pb-2 ps-5 pe-5">Signalétique</div>
+            </div>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="container pt-5 pb-5">
+        <div class="row position-relative">
+            <div class="col-3">
+                <h2 class="nord mb-3">Developpement</h2>
+                <p class="lex">Du wireframe en passant par les recettes jusqu'à la mise en ligne, le pôle développement apporte la réponse technique à vos problématiques digitales.</p>
+            </div>
+            <div class="col-3"></div>
+            <div class="col-3"></div>
+            <div class="col-12 bg-light rounded-4">
+                <div class="col-4 p-5">
+                    <h3 class="nord">Site internet, vitrine et e-commerce</h3>
+                    <p class="lex">Point d'entrée principal pour de nombreuses entreprises, le site internet, qu'il soit vitrine ou e-commerce, reste un sujet complexe et opaque dans son élaboration. Notre équipe vous accompagne dans sa réalisation et son déploiement sans utiliser de mots incompréhensibles.</p>
+                </div>
+                <div class="col-3"></div>
+                <div class="col-3"></div>
+            </div>
+            <img src="../assets/images/exemple-developpement.png" id="exemple-developpement" class="position-absolute img-fluid">
+        </div>
+    </div>
+</section>
+<section>
+    <div class="container pt-5 pb-5 position-relative">
+        <div class="row mb-3">
+            <div class="col-3"></div>
+            <div class="col-6 text-center">
+                <h2 class="nord mb-3">Webmarketing</h2>
+                <p class="lex">Regroupe les différents leviers participant au développement de votre activité afin de répondre à vos attentes de visibilité, communication, chiffre d'affaire ...</p>
+            </div>
+            <div class="col-3"></div>
+        </div>
+        <div class="row mb-5">
+            <div class="col-2"></div>
+            <div class="col-8 d-flex align-items-center justify-content-center bg-light p-5 rounded-3">
+                <div class="col lex d-flex align-items-center justify-content-end pe-5 fw-bold">
+                    <ul>
+                        <li>Réseaux sociaux</li>
+                        <li>Webmastering</li>
+                        <li>SEO et SEA</li>
+                        <li>CRM</li>
+                    </ul>
+                </div>
+                <div class="col lex d-flex align-items-center justify-content-start ps-5 fw-bold">
+                    <ul>
+                        <li>Emailings</li>
+                        <li>Influance marketing</li>
+                        <li>SMA</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-2"></div>
+        </div>
+        <div class="row">
+            <div class="col-6"></div>
+            <div class="col-6 text-end">
+                <h3 class="nord">Atteignez vos objectifs...<br>Faites le bon choix</h3>
+                <p class="lex">Perdu dans le choix du levier à activer ou manque de temps pour réaliser votre community management ? Chez Dimension on a parfaitement conscience que cela peut paraître complexe... Ou chronophage ! Notre équipe vous accompagne et surtout vous éclaire sur toutes ces questions.</p>
+            </div>
+        </div>
+        <img src="../assets/images/expertise_webmarketing.png" class="position-absolute" id="webmarketing-img">
+    </div>
+</section>
+<section>
+<div class="container p-5">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h2 class="nord text-center">Convaincu ? Alors n'hesitez plus !</h2>
+                <button class="button bg-light mt-5">Contactez-nous</button>
+            </div>
+        </div>
+    </div>
+</section>
+
 
 <?php $content = ob_get_clean();?>
 <?php require('../templates/layout.php') ?>
